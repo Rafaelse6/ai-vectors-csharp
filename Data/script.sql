d1f3e54cc6fd4cdb8eb8df2285b77136c4396443
Rafael Santos <rafa.05125@gmail.com>
2026-06-07 17:51:45 -0300

    Initial queries

--- Data/script.sql
+++ Data/script.sql
@@ -1,1 +1,9 @@
 --docker run -d -p 5432:5432 -e POSTGRES_PASSWORD=postgres pgvector/pgvector:pg17   
+--CREATE EXTENSION IF NOT EXISTS vector;
+
+-- CREATE TABLE recomendations (
+--     id SERIAL PRIMARY KEY,
+--     title TEXT NOT NULL,
+--     category TEXT NOT NULL,
+--     embedding vector(1024) NOT NULL
+-- );
